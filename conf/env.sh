2e202f5508e462e91e0cd9f6ff5e2cf55af1326a
Scott Michael <scamicha@iu.edu>
2014-04-01 14:22:13 -0400

    Added comments to env.sh for what needs to be set

--- conf/env.sh
+++ conf/env.sh
@@ -4,22 +4,26 @@
 export HADOOP_ROOT=/N/u/scamicha/BigRed2/hadoop/default
 
 # What filesystem to use? Choices are hdfs and lustre
+# If lustre is chosen set HADOOP_LOCAL to true and make
+# sure that HADOOP_DIR is accessible from all the nodes 
 export HADOOP_FS=lustre
 
-# Local directory to use for HDFS
+# Local directory to use for HDFS or lustre
 export HADOOP_DIR=/N/dc2/scratch/scamicha/HADOOP_LUSTRE
 
 #Globally accessible directory to copy logs to
 export HADOOP_FINAL_LOG="$PBS_O_WORKDIR/$PBS_JOBID.hadoop.log"
 
 # Is the data store node local?
+# Set to true to use node local storage with HDFS or to use
+# native lustre storage. For HDFS on lustre set to false and
+# be sure to set HADOOP_GLOBAL
 export HADOOP_LOCAL=true
 
 # If HADOOP_LOCAL is false then this must be set to the 
 # globally accessible scratch directory you want to map to
 if [ ! "$HADOOP_LOCAL" = true ]; then
-    export HADOOP_GLOBAL_DATA=/N/dc2/scratch/scamicha/HPCHADOOP_TEST/DATA
-    export HADOOP_GLOBAL_LOG=/N/dc2/scratch/scamicha/HPCHADOOP_TEST/LOG
+    export HADOOP_GLOBAL=/N/dc2/scratch/scamicha/HPCHADOOP_TEST/
 fi
 
 # Starting port to search
@@ -37,3 +41,7 @@ export HADOOP_MAX_REDS=32
 export HADOOP_DATA=$HADOOP_DIR/data
 export HADOOP_LOG=$HADOOP_DIR/log
 export HADOOP_MAPRED=$HADOOP_DIR/mapred
+if [ ! "$HADOOP_LOCAL" = true ]; then
+    export HADOOP_GLOBAL_DATA=$HADOOP_GLOBAL/data
+    export HADOOP_GLOBAL_LOG=$HADOOP_GLOBAL/log
+fi
